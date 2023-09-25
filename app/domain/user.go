bd8b7b0c744cd38205d52cbafdb32437f8dfccb3
nonils <nonils@tutanota.com>
2023-09-25 01:45:31 -0300

    Starts to load env vars

--- app/domain/user.go
+++ app/domain/user.go
@@ -1,17 +1,24 @@
 package domain
 
-import "time"
+import (
+	"gorm.io/gorm"
+	"time"
+)
 
 type User struct {
-	ID               string    `json:"id"`
-	Name             string    `json:"name"`
-	Email            string    `json:"email"`
-	LastLogin        time.Time `json:"lastLogin"`
-	CreatedAt        time.Time `json:"createdAt"`
-	UpdatedAt        time.Time `json:"updatedAt"`
-	DeletedAt        time.Time `json:"deletedAt"`
-	BadLoginAttempts int       `json:"badLoginAttempts"`
-	IsLocked         bool      `json:"isLocked"`
-	IsSocialLogin    bool      `json:"isSocialLogin"`
-	Hash             string
+	ID               string         `json:"id" gorm:"column:id;primaryKey"`
+	Name             string         `json:"name" gorm:"column:name"`
+	Email            string         `json:"email" gorm:"column:email;uniqueIndex"`
+	LastLogin        time.Time      `json:"lastLogin" gorm:"column:last_login"`
+	CreatedAt        time.Time      `json:"createdAt" gorm:"column:created_at"`
+	UpdatedAt        time.Time      `json:"updatedAt" gorm:"column:updated_at"`
+	DeletedAt        gorm.DeletedAt `json:"deletedAt" gorm:"column:deleted_at;index"`
+	BadLoginAttempts int            `json:"badLoginAttempts" gorm:"column:bad_login_attempts"`
+	IsLocked         bool           `json:"isLocked" gorm:"column:is_locked"`
+	IsSocialLogin    bool           `json:"isSocialLogin" gorm:"column:is_social_login"`
+	Hash             string         `gorm:"column:hash"`
+}
+
+func (User) TableName() string {
+	return "Users"
 }
